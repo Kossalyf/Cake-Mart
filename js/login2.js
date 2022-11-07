bb25b53d4bfcda180c0e9fb85b5fdba5b02ae37e
Fariza <fkosaly@gmail.com>
2022-11-07 19:28:42 +0600

    init commit

---
+++ js/login2.js
@@ -0,0 +1,18 @@
+  
+ document.getElementById('check').onclick = function(){
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+  
+    if(email =='---' && password =='12345')
+    alert('Welcome');
+    else if(email =='---' && password =='12345')
+    alert('Welcome');
+    else if(email ==' ---' && password =='12345')
+    alert('Welcome');
+    else if(email =='--' && password =='12345')
+    alert('Welcome');
+    else alert('Incorrect login or pass');
+ }
+ document.getElementById('go')
+  .addEventListener('click', () => window.open('file:///C:/Users/%D0%90%D0%B4%D0%BC%D0%B8%D0%BD/Downloads/project%20rk1%20web/project%20rk1%20web/aftindex.html?'));
+
